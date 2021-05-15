274d63f0bd7dcd752b3917088def6278b100f3ca
Felipe Oliveira <fsousa@tasken.com>
2021-05-14 21:11:22 -0300

    feature: Configurando banco de dados.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [UserModule, ConfigModule.forRoot()],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
